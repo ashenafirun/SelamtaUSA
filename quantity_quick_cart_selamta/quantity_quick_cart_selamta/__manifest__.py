bff73bcbf9e718d6f159ce5e894eaaa047c3327d
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2022-02-04 01:19:24 -0500

    Add files via upload

---
+++ quantity_quick_cart_selamta/quantity_quick_cart_selamta/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Quantity Quick Cart Selamta",
+    "category": "Website",
+    "version": "15.0.1",
+    "depends": ['theme_alan', 'stock', 'website_sale_stock'],
+    "data": [
+        'views/sh_shop_template.xml',
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "/quantity_quick_cart_selamta/static/src/js/shop.js",
+            "/quantity_quick_cart_selamta/static/src/scss/shop.scss",
+        ],
+    },
+    'licence': 'LGPL-3',
+    "auto_install": True,
+    "application": True,
+    "installable": True,
+}
