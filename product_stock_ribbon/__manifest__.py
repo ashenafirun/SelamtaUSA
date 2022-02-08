93413ecae753f0b160119c397e10ee027c53ea56
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2022-02-08 12:22:46 -0500

    Add files via upload

---
+++ product_stock_ribbon/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Shop Page Product Ribbon",
+    "category": "Website",
+    "version": "15.0.1",
+    "depends": ['theme_alan', 'website_sale_stock'],
+    "data": [
+        'views/shop_page.xml',
+    ],
+    "assets": {
+    },
+    'licence': 'LGPL-3',
+    "auto_install": True,
+    "application": True,
+    "installable": True,
+}
