a8785bffb0500f9df75a6546e7e6423a422b494c
awais syed <sma@numla.com>
2025-05-01 01:01:46 +0500

    [FIX:] Product creation fix for advance_website_setting

--- advance_website_settings/models/product_template.py
+++ advance_website_settings/models/product_template.py
@@ -12,25 +12,31 @@ class ProductTemplateInherit(models.Model):
     @api.model_create_multi
     def create(self,vals):
         rec = super(ProductTemplateInherit, self).create(vals)
-        if vals["minimum_product_quantity"] or vals["max_product_quantity"]:
-            if (vals["minimum_product_quantity"] > vals["max_product_quantity"]) and vals["max_product_quantity"] != -1 :
-                raise ValidationError("Maximum product quantity should be more than minimum product qunatity")
-        
-            elif vals["minimum_product_quantity"] < -1 or vals["max_product_quantity"] < -1:
-                raise ValidationError(" Value must be greater then -2")
-        if vals["minimum_product_quantity"]==0 or vals["max_product_quantity"]==0:
-            raise ValidationError("Product quantity should not be zero. ")
+        for val in vals:
+            if 'minimum_product_quantity' in val and 'max_product_quantity' in val:
+                if val["minimum_product_quantity"] or val["max_product_quantity"]:
+                    if (val["minimum_product_quantity"] > val["max_product_quantity"]) and val["max_product_quantity"] != -1 :
+                        raise ValidationError("Maximum product quantity should be more than minimum product qunatity")
+
+                    elif val["minimum_product_quantity"] < -1 or val["max_product_quantity"] < -1:
+                        raise ValidationError(" Value must be greater then -2")
+                if val["minimum_product_quantity"]==0 or val["max_product_quantity"]==0:
+                    raise ValidationError("Product quantity should not be zero. ")
         return rec
 
     def write(self,vals):
-        rec = super(ProductTemplateInherit,self).write(vals)
-        if self.minimum_product_quantity or self.max_product_quantity:
-            if (self.minimum_product_quantity > self.max_product_quantity) and self.max_product_quantity != -1 :
-                raise ValidationError("Maximum product quantity should be more than minimum product qunatity")
-            elif self.minimum_product_quantity < -1 or self.max_product_quantity < -1:
-                raise ValidationError(" Value must be greater then -2")
-        if self.minimum_product_quantity == 0 or self.max_product_quantity == 0:
-            raise ValidationError("Product quantity should not be zero. ")
+        rec = super(ProductTemplateInherit, self).write(vals)
+        for val in vals:
+            if 'minimum_product_quantity' in val and 'max_product_quantity' in val:
+                if val["minimum_product_quantity"] or val["max_product_quantity"]:
+                    if (val["minimum_product_quantity"] > val["max_product_quantity"]) and val[
+                        "max_product_quantity"] != -1:
+                        raise ValidationError("Maximum product quantity should be more than minimum product qunatity")
+
+                    elif val["minimum_product_quantity"] < -1 or val["max_product_quantity"] < -1:
+                        raise ValidationError(" Value must be greater then -2")
+                if val["minimum_product_quantity"] == 0 or val["max_product_quantity"] == 0:
+                    raise ValidationError("Product quantity should not be zero. ")
         return rec
     
     @api.onchange('check_box')
