0ca0d443dc4f7e306478f76338d8d987c46388de
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2026-05-14 09:54:26 -0400

    Delete sync_payment_adyen_charge directory

--- sync_payment_adyen_charge/README.md
+++
@@ -1,3 +0,0 @@
-# Adyen Charge
-
-Payment Adyen Charge (Fee)
--- sync_payment_adyen_charge/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
-
-from . import account_payment_register
-from . import payment_link_wizard
