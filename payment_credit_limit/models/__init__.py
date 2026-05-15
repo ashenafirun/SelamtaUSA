2639a00ea1132d505a1263e53affb35efa9848fd
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2026-05-15 02:42:32 -0400

    Delete payment_credit_limit directory

--- payment_credit_limit/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import sale
-from . import credit_code
-from . import res_partner
-from . import account_move
-from . import res_confing_settings
