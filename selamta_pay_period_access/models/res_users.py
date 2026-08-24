ca169a5d9ab11329f89c6eaacb73ee9247bedba7
ashenafirun <79759843+ashenafirun@users.noreply.github.com>
2026-08-24 14:36:30 -0400

    Add files via upload

---
+++ selamta_pay_period_access/models/res_users.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+PAY_PERIOD_FIELDS = [
+    'x_hours_current_pay_period',
+    'x_hours_last_pay_period',
+]
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + PAY_PERIOD_FIELDS
